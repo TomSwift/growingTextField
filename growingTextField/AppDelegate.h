3388d1d21e6972f916a39941b4bb1bcb9013613a
Nick <nick@remitly.com>
2017-04-25 15:24:04 -0700

    Initial Commit

---
+++ growingTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  growingTextField
+//
+//  Created by Nicholas Hodapp on 4/25/17.
+//  Copyright © 2017 CoDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
